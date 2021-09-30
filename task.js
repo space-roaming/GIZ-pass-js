f2c46c04b2bafeb078ef13b17447d27e5383bdcc
space-roaming <space.roaming27@gmail.com>
2021-09-30 20:46:37 +0300

    init

--- task.js
+++ task.js
@@ -1,44 +1,32 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
-
-console.log("Original numbers list: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+let numbers  = [5,8,0,1,9,11,15,16]
+    console.log("orginal numbers" , numbers)
+    for ( let i = 0 ; i < numbers.length; i++)
     {
-        if(numbers[i] > numbers[j + 1])
+        for( let j = 0 ; j <numbers.length  ; j++)
         {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+            if(numbers[j] > numbers[j+1])
+            {
+                let temp = numbers[j];
+                numbers[j] = numbers [j+1]
+                numbers[j+1] = temp;
+
+            }
         }
     }
-}
 
 console.log("Numbers list After sorting: ", numbers)
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
-}
-
-console.log("Numbers list After Desc sorting: ", numbers)
-
+for(let i=0; i<numbers.length ; i++)
+  {
+      for(let j = 0 ; j<numbers.length;  j++)
+      {
+          if(numbers[j] < numbers[j+1])
+          {
+              let temp = numbers[j];
+              numbers[j] = numbers[j+1]
+              numbers[j+1] = temp ;
+          }
+      }
+  }
+  console.log("numbers list after desc sorting " , numbers)
 
